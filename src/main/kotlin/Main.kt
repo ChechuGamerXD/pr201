60772c03656abc2df275f3e5c7aabef405510dcc
jesusandresconfedora <jesusandresaledohernandez@gmail.com>
2023-10-06 16:44:11 +0200

    Respaldo

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+    val lado = 50
+    val superficie = lado * lado
+    val perimetro = lado * 4
+    println("Superficie del cuadrado: $superficie\nPerímetro del cuadrado: $perimetro")
+}
